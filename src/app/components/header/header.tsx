74b28f9a27810837e7e8067aeadbd89cfd059e23
João Victor <joaofdias.dev@gmail.com>
2024-08-13 09:20:35 -0300

    atlerações feitas no scss

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -3,17 +3,17 @@ import Image from "next/image";
 export function Header(){
     return(
         <div className="header">
-         <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              width={100}
-              height={24}
-              priority
-            />
             <div>
               <h1>Hi, i´m João Victor!</h1>
               <h2>Web developer</h2>
             </div>
+            <Image
+                  src="/vercel.svg"
+                  alt="Vercel Logo"
+                  width={100}
+                  height={24}
+                  priority
+                />
       </div>
     )
 }
