249e004d31d919810d395709f4ee5376f294ed8b
João Victor <joaofdias.dev@gmail.com>
2024-11-25 21:41:45 -0300

    feat: add initial html strucuture and header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,24 @@
-import type { Metadata } from "next";
-import { Poppins} from "next/font/google";
-import "./globals.scss";
+import './globals.scss'
+import { Poppins } from 'next/font/google'
 
-const inter = Poppins({
-  subsets: ["latin"], 
-  weight: ['400', '500', '600'] 
-});
+const inter = Poppins({ 
+  subsets: ['latin'], 
+  weight: ['300', '400', '500', '600']
+})
 
-export const metadata: Metadata = {
-  title: "João Victor",
-  description: "João Victor is a software engineer...",
-};
+export const metadata = {
+  title: 'Fernanda Kipper',
+  description: 'Fernanda Kipper is a software engineer...',
+}
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
